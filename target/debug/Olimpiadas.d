d91a055df8c0112fb9bbc18241701005c984b244
DannyAJJ <dannyalejandro.jj@gmail.com>
2023-11-25 14:13:44 -0400

    vacio

---
+++ target/debug/Olimpiadas.d
@@ -0,0 +1,1 @@
+C:\Users\58424\Documents\Proyectos\Rust\Olimpiadas\target\debug\Olimpiadas.exe: C:\Users\58424\Documents\Proyectos\Rust\Olimpiadas\src\main.rs
---
+++ target/debug/deps/Olimpiadas.d
@@ -0,0 +1,5 @@
+C:\Users\58424\Documents\Proyectos\Rust\Olimpiadas\target\debug\deps\Olimpiadas.exe: src\main.rs
+
+C:\Users\58424\Documents\Proyectos\Rust\Olimpiadas\target\debug\deps\Olimpiadas.d: src\main.rs
+
+src\main.rs:
